88c6e90b84fa2dd59a128c06ab8e4d37b06f4049
MertSaygili <mert71719601@gmail.com>
2022-09-23 23:11:38 +0300

    opened database_manager

--- lib/SQFlite/core/database/database_manager.dart
+++ lib/SQFlite/core/database/database_manager.dart
@@ -1,1 +1,29 @@
-class DatabaseManager {}
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseManager {
+  Database? _database;
+
+  final String _userTableName = 'employee';
+  final int _version = 1;
+
+  Future<Database> get database async {
+    final databasePath = await getDatabasesPath();
+    const databaseName = 'employee.db';
+
+    final path = join(databasePath, databaseName);
+
+    _database = await openDatabase(
+      path,
+      version: _version,
+      onCreate: _createDatabase,
+    );
+
+    return _database!;
+  }
+
+  // creates database table
+  Future<void> _createDatabase(Database db, int version) async {
+    await db.execute('''''');
+  }
+}
