e6775cc845eca30cea4bc543e37f529160ab3511
MertSaygili <mert71719601@gmail.com>
2022-09-23 23:05:20 +0300

    updated employee_model

--- lib/SQFlite/core/model/employee_model.dart
+++ lib/SQFlite/core/model/employee_model.dart
@@ -1,43 +1,34 @@
 class EmployeeModel {
   int? id;
-  String? name;
-  String? surname;
-  String? phoneNumber;
-  String? eMail;
-  bool? gender;
-  DateTime? entryYear;
+  String name;
+  String surname;
+  String department;
+  String phoneNumber;
+  String eMail;
+  bool gender;
+  DateTime entryYear;
 
   EmployeeModel({
     this.id,
-    this.name,
-    this.surname,
-    this.phoneNumber,
-    this.eMail,
-    this.gender,
-    this.entryYear,
+    required this.name,
+    required this.surname,
+    required this.department,
+    required this.phoneNumber,
+    required this.eMail,
+    required this.gender,
+    required this.entryYear,
   });
 
-  EmployeeModel.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    surname = json['surname'];
-    phoneNumber = json['phoneNumber'];
-    eMail = json['eMail'];
-    gender = json['gender'];
-    entryYear = json['entryYear'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-
-    data['id'] = id;
-    data['name'] = name;
-    data['surname'] = surname;
-    data['phoneNumber'] = phoneNumber;
-    data['eMail'] = eMail;
-    data['gender'] = gender;
-    data['entryYear'] = entryYear;
-
-    return data;
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'name': name,
+      'surname': surname,
+      'department': department,
+      'phoneNumber': phoneNumber,
+      'eMail': eMail,
+      'gender': gender ? 1 : 0, // 1 is woman, 0 is men
+      'entryYear': entryYear.toString(), // sqflite doest not support date time
+    };
   }
 }
