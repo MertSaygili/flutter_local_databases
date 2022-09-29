a6583b433729269aeda385353a4f13fd5844063c
MertSaygili <mert71719601@gmail.com>
2022-09-29 19:56:17 +0300

    we took name with id

--- lib/SQFlite/view/update_employee_page.dart
+++ lib/SQFlite/view/update_employee_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_databases/SQFlite/core/database/database_manager.dart';
-import 'package:flutter_local_databases/SQFlite/core/model/employee_model.dart';
 import 'package:flutter_local_databases/SQFlite/widgets/custom_appbar.dart';
 import 'package:flutter_local_databases/SQFlite/widgets/custom_floatingactionbutton.dart';
 import 'package:flutter_local_databases/SQFlite/widgets/custom_search_textfield.dart';
 import 'package:flutter_local_databases/constants/constanst.dart';
+
+import '../core/model/employee_model.dart';
 
 class UpdateEmployeePageView extends StatefulWidget {
   const UpdateEmployeePageView({super.key, required this.db});
@@ -16,7 +17,8 @@ class UpdateEmployeePageView extends StatefulWidget {
 }
 
 class _UpdateEmployeePageViewState extends State<UpdateEmployeePageView> {
-  late final String _name;
+  late EmployeeModel _employeeModel;
+  late int _id;
 
   @override
   Widget build(BuildContext context) {
@@ -35,8 +37,15 @@ class _UpdateEmployeePageViewState extends State<UpdateEmployeePageView> {
     );
   }
 
-  void _takeEmployeeName(String name) =>
-      setState(() => _name = name.split('.')[1]);
+  void _takeEmployeeName(String id) {
+    int searchId = int.tryParse(id.split('.')[1]) ?? 1;
+    // show alert if searchId is null
 
-  void _takeEmployeeInformation(String id) {}
+    setState(() => _id = searchId);
+  }
+
+  void _takeEmployeeInformation() async {
+    _employeeModel = await widget.db.searchById(_id);
+    print(_employeeModel.name);
+  }
 }
